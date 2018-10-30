46a66509e2ea1e1d9d65cd16416c8a29be3b94b9
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-10-30 17:30:54 +0100

    Export getOSinfo function to OSinfo.js

--- modules.js
+++ modules.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var OSinfo = require('./OSinfo');
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
@@ -20,7 +21,7 @@ process.stdin.on('readable', function() {
                 break;
 
             case 'getOSinfo':
-                getOSinfo();
+                OSinfo.print();
                 break;
 
             // if user writes not mentioned command
@@ -29,22 +30,3 @@ process.stdin.on('readable', function() {
         };
     }
 });
-
-function getOSinfo() {
-    var type = os.type();
-    if(type === 'Darwin') {
-        type = 'OSX';
-    } else if(type === 'Windows_NT') {
-        type = 'Windows';
-    }
-    var release = os.release();
-    var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
-    var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
-}
