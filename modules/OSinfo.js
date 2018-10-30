afe26fee5133dbe73fbae98d49a9721316d5bcb2
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-10-30 18:49:27 +0100

    Create module for time format

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var timeFormat = require('../modules/timeFormat');
 
 function getOSinfo() {
     var type = os.type();
@@ -9,14 +10,13 @@ function getOSinfo() {
     }
     var release = os.release();
     var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
     var userInfo = os.userInfo();
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
+    timeFormat.timeFormat();
 }
 
 exports.print = getOSinfo;
---
+++ modules/timeFormat.js
@@ -0,0 +1,26 @@
+var os = require('os');
+
+function timeFormat() {
+    var uptime = os.uptime();
+
+    // minutes and hours base counting
+    var hrs = parseInt(uptime / 3600); // hrs counting
+    var scsLeftHrs = (uptime % 3600); // seconds left after hrs counting
+
+    var mins = parseInt(scsLeftHrs / 60); // mins counting
+    var secs = parseInt (scsLeftHrs % 60); // seconds left after mins counting
+
+    if (mins > 0) {
+        if (hrs > 0) {
+            {console.log(`Uptime: ${hrs} hrs ${mins} mins ${secs} secs`)}
+        }
+        else {
+            {console.log(`Uptime: ${mins} mins ${secs} secs`)}
+        }
+    } 
+    else { 
+    console.log(`Uptime: ${secs} secs`)
+    }
+}
+
+exports.timeFormat = timeFormat;
