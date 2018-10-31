4dd792263709fa53d39aac136fbbf0bdea2588c3
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-10-31 18:31:09 +0100

    Serve the files!

--- http-server.js
+++ http-server.js
@@ -1,28 +1,23 @@
-var fs = require('fs');
-var colors = require('colors');
+var fs = require ('fs');
+var http = require('http');
 
-// old function for files
+// same meaning:
+// var server = http.createServer();
+// server.on('request', function (request, response) {
 
-/* fs.readFile('./text.txt', 'utf-8', function(err, data) {
-    console.log('Data before changes'.blue);
-    console.log(data);
-    fs.appendFile('./text.txt', 'There is some another text', function(err) {
-        if (err) throw err;
-        console.log('Okay!'.green);
-        fs.readFile('./text.txt', 'utf-8', function(err, data) {
-            console.log('Data after saving'.blue);
-            console.log(data);
+var server = http.createServer(function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/url') {
+        fs.readFile('./index.html', function (err, data) {
+            response.write(data);
+            response.end();
         });
-    });
-}); */
+        
+    } else {
+            response.statusCode = 404;
+            response.write('<img src="https://image.freepik.com/darmowe-wektory/zabawny-błąd-404-projekt-tła_1167-219.jpg" alt="404">');
+            response.end();
+    }
+});
 
-
-
-// new function for folder
-
-fs.readdir('./testFolder', function (err, data) {
-    console.log('Folders content was read'.cyan);
-    fs.writeFile('./testFolder/generatedFile.txt', data, function(){
-        console.log('Success! File was saved.'.green);
-    })
-})
+server.listen(8080);
